7fcdcaea0e9e1cb8d4bb030333d41655608054ec
Gabriel Cascaes <gabriel.cascaes@harbor.com.br>
2021-05-23 16:31:46 -0300

    Melhorias no pulo do jogador.

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -6,18 +6,20 @@ public class InputController : MonoBehaviour
 
     private float move = 0;
     private bool shouldJump = false;
+    private bool holdJump = false;
 
     private void Update()
     {
         move = Input.GetAxisRaw("Horizontal");
         if (Input.GetButtonDown("Jump"))
             shouldJump = true;
+        holdJump = Input.GetButton("Jump");
     }
 
     private void FixedUpdate()
     {
-        if (move != 0 || shouldJump)
-            movementController.SmoothMove(move, shouldJump);
+        if (move != 0 || shouldJump || holdJump)
+            movementController.SmoothMove(move, shouldJump, holdJump);
 
         shouldJump = false;
     }

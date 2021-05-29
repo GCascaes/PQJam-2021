7ac7ad5fdbfa2501541854a35d049df28549d031
Gabriel Cascaes <gabriel.cascaes@harbor.com.br>
2021-05-29 17:54:04 -0300

    Implementada morte simples no controlador de vida.

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -4,12 +4,21 @@ public class HealthController : MonoBehaviour
 {
     [SerializeField]
     private float MaxHealth;
+    [SerializeField]
+    private bool shouldDie;
 
     private float currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = MaxHealth;
+    }
 
     public void TakeDamage(float damage)
     {
         Debug.Log("Taking damage");
         currentHealth -= damage;
+        if (shouldDie && currentHealth <= 0)
+            Destroy(gameObject);
     }
 }

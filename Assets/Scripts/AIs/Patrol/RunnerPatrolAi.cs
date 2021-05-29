3b1e93de123b28977e71add591c17961268aa2a5
Gabriel Cascaes <gabriel.cascaes@harbor.com.br>
2021-05-29 19:18:00 -0300

    Adicionado script de spawner, runner e death hole;

---
+++ Assets/Scripts/AIs/Patrol/RunnerPatrolAi.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RunnerPatrolAi : MonoBehaviour, IPatrolMovementAi
+{
+    private bool isRunning = false;
+
+    private IEnumerator runCoroutine;
+    private GroundMovementController movementController;
+
+    private void Awake()
+    {
+        movementController = GetComponent<GroundMovementController>();
+        runCoroutine = Run();
+    }
+
+    public void StartPatroling()
+    {
+        isRunning = true;
+        StopAllCoroutines();
+        StartCoroutine(runCoroutine);
+    }
+
+    public void StopPatroling()
+    {
+        isRunning = false;
+        StopAllCoroutines();
+    }
+
+    private IEnumerator Run()
+    {
+        if (movementController is null)
+            yield break;
+
+        while (isRunning == true)
+        {
+            movementController.SmoothMove(movementController.FacingRight ? 1 : -1, false, false);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject spawnPrefab;
+    [SerializeField]
+    private float spawnPeriod;
+
+    private void Awake()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator Spawn()
+    {
+        while (isActiveAndEnabled)
+        {
+            Instantiate(spawnPrefab, transform.position, transform.rotation);
+            yield return new WaitForSecondsRealtime(spawnPeriod);
+        }
+    }
+}
